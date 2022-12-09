0d8bce18a7586770b93b02d61f0031c98008fc00
Victoria <viccoshe@gmail.com>
2022-12-09 18:08:18 +0300

    catalogue added

--- src/components/Catalogue/Catalogue.tsx
+++ src/components/Catalogue/Catalogue.tsx
@@ -1,3 +1,4 @@
+import { MouseEvent } from "react";
 //@ts-ignore
 import style from "./Catalogue.module.css";
 import  { useEffect, useState } from "react";
@@ -5,10 +6,26 @@ import  { useEffect, useState } from "react";
 import Bin from "../../img/musor.svg";
 //@ts-ignore
 import Like from "../../img/heart.svg";
+import { Routes, Route } from "react-router-dom";
+//@ts-ignore
+import Loader from "../../utiles/Loader/Loader";
+
 
 const Catalogue: React.FC = () => {
-    //const [loading, setLoading] = useState(true);
-    const [catalogue, setCatalogue] = useState<any[]>([]);
+    //const products: Array<any> = [];
+    const [loading, setLoading] = useState<boolean>(true);
+    const [catalogue, setCatalogue] = useState<any>([]);
+    const [selectedCategory, setSelectedCategory] = useState<any>([]);
+
+    const getCategory = (cat: string) =>{
+        const filteredResult: Array<{}> = catalogue.filter((item: { category: string}, i: string) => {
+            if(item.category === cat){
+                return item;
+            }
+        })
+        console.log(filteredResult);
+        setSelectedCategory(filteredResult);
+    }
 
     useEffect(() => {
         fetch('https://fakestoreapi.com/products')
@@ -19,52 +36,86 @@ const Catalogue: React.FC = () => {
             }) 
             .then(data => {
                 setCatalogue(data);
+                setLoading(false);
             })
     }, [])
 
 
-    return (
+    if (loading) {
+        return (
+            <main style={{
+                minHeight: '70vh',
+                height: 'fit-content',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'}}>
+               <div><Loader/></div> 
+            </main>
+            
+        )
+    }else return ( 
         <main className={style.main}>
             <div className={style.container}>
                 <h3>Catalogue</h3>
                 <div className={style.cat}>
-                    <ul className={style.catBlock}>
-                        <li className={style.active}>Living room</li>
-                        <li>Bedroom</li>
-                        <li>Wardrobe</li>
-                        <li>Playroom</li>
-                    </ul>
-                    <ul className={style.catBlock}>
-                        <li>Hallway</li>
-                        <li>Textile</li>
-                        <li>Decor</li>
-                    </ul>
+                    <div onClick={() => {setSelectedCategory([])}} className={style.productCategory}>All</div>
+                    <div onClick={() => {getCategory(`men's clothing`)}} className={style.productCategory}>Men</div>
+                    <div onClick={() => {getCategory(`women's clothing`)}} className={style.productCategory}>Women</div>
+                    <div onClick={() => {getCategory('electronics')}} className={style.productCategory}>Electronics</div>
+                    <div onClick={() => {getCategory('jewelery')}} className={style.productCategory}>Jewelery</div>
                 </div>
 
                 <div className={style.productsContainer}>
-                    {catalogue.length > 0 ?
-                        catalogue.map((item, i) => {
+
+                    {selectedCategory.length > 0 
+                    
+                    ? selectedCategory.map((item: any, i: any) => {
                             console.log(item);
-                            return <div key={i} className={style.productItem}> 
+                            const {id, title, price, description: desc, category: cat, image} = item;
+                            return <div key={id} className={style.productItem}> 
                                 <div className={style.img}>
-                                    <div className={style.productCat}>{item.category}</div>
-                                    <div className={style.img}><img src={item.image} alt="product"/></div>
+                                    <div className={style.productCat}>{cat}</div>
+                                    <div className={style.img}><img src={image} alt="product"/></div>
                                 </div>
                                 <div className={style.productInfo}>
-                                    <h5>{item.title}</h5>
-                                    <p className={style.productDesc}>{item.description}</p>
-                                    <p className={style.productPrice}>{item.price} $</p> 
+                                    <h5>{title}</h5>
+                                    <p className={style.productDesc}>{desc}</p>
+                                    <p className={style.productPrice}>{price} $</p> 
                                     <div className={style.buttons}>
-                                <div className={style.fav}><img src={Like} alt="" /></div>
+                                    <div className={style.fav}><img src={Like} alt="like" /></div>
+                                    <div className={style.minus}>-</div>
+                                    <div className={style.counter}>0</div>
+                                    <div className={style.plus} >+</div>
+                                    <div className={style.remove} ><img src={Bin} alt="remove" /></div>
+                                </div>
+                            </div> 
+                        </div>
+                        })
+                    :  selectedCategory.length <= 0 && catalogue.length > 0
+                    
+                    ? catalogue.map((item: any, i: any) => {
+                        console.log(item);
+                        const {id, title, price, description: desc, category: cat, image} = item;
+                        return <div key={id} className={style.productItem}> 
+                            <div className={style.img}>
+                                <div className={style.productCat}>{cat}</div>
+                                <div className={style.img}><img src={image} alt="product"/></div>
+                            </div>
+                            <div className={style.productInfo}>
+                                <h5>{title}</h5>
+                                <p className={style.productDesc}>{desc}</p>
+                                <p className={style.productPrice}>{price} $</p> 
+                                <div className={style.buttons}>
+                                <div className={style.fav}><img src={Like} alt="like" /></div>
                                 <div className={style.minus}>-</div>
                                 <div className={style.counter}>0</div>
                                 <div className={style.plus} >+</div>
-                                <div className={style.remove} ><img src={Bin} alt="" /></div>
+                                <div className={style.remove} ><img src={Bin} alt="remove" /></div>
                             </div>
-                                </div> 
-                        </div>
-                        })
-                    : <h3>THERE IS AN ERROR</h3>
+                        </div> 
+                    </div>
+                    })
+                    : <h3>THERE'S AN ERROR :C</h3>
                     }
                 </div>
 
---
+++ src/utiles/Loader/loader.tsx
@@ -0,0 +1,10 @@
+// @ts-ignore
+import style from "./Loader.module.css";
+
+const Loader: React.FC = () => {
+    return (
+        <div className={style.ldsRoller}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+    )
+}
+
+export default Loader;
